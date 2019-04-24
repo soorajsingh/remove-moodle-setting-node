c26f8da97b0d427392db66b08f37c46946ed12f4
Sooraj singh <37901471+soorajsingh@users.noreply.github.com>
2019-04-24 22:48:11 +0530

    Update lib.php

--- lib.php
+++ lib.php
@@ -6,4 +6,23 @@ function local_module_event_extend_settings_navigation($settingsnav, $context) {
 			$settingnode->remove();
 		}
 	}
+function local_subscriptions_extend_navigation(global_navigation $navigation)
+    {
+        global $CFG,$DB,$USER, $PAGE;
+		
+			
+				$navigation->add("Course Subscriptions",new moodle_url("/local/subscriptions/userplan.php"));
+				if(is_siteadmin())
+				{
+    				$nav=$navigation->add("Course Bundle");
+    				$nav->add("Bundle List",new moodle_url("/local/subscriptions/bundle_list.php"));
+    				$nav->add("Add New Bundle",new moodle_url("/local/subscriptions/edit_bundle.php"));
+				}
+				$navigation->add("Subscriptions Plan",new moodle_url("/local/subscriptions/subscriptionplan.php"));
+                if ($settingnode = $navigation->find('participants', navigation_node::TYPE_CONTAINER)) { 
+                    //print_object($navigation);
+        			$settingnode->remove();
+        		}
+		
+	}
    ?>
